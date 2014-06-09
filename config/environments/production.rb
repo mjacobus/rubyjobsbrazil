ed25c88531c49625d3bba7f01db70f5a8b8d1c97
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-06-09 16:00:26 -0300

    Changed url of production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'rubyjobsbrasil.com' }
+  config.action_mailer.default_url_options = { host: 'rubyjobsbrasil.com.br' }
 end
