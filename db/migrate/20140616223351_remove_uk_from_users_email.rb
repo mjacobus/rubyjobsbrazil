15ff859de023dce0e1ce985069c09e8d144fe303
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-06-22 19:02:25 -0300

    Fixed migration

--- db/migrate/20140616223351_remove_uk_from_users_email.rb
+++ db/migrate/20140616223351_remove_uk_from_users_email.rb
@@ -1,9 +1,13 @@
 class RemoveUkFromUsersEmail < ActiveRecord::Migration
   def up
     remove_index :users, :email
+  rescue
+    puts "table users does not exist"
   end
 
   def down
     add_index :users, :email, unique: true
+  rescue
+    puts "table users does not exist"
   end
 end
