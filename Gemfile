afbe0db8bb6a4759387e17f3b57a91a85d690098
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-08-02 19:20:21 +0200

    Migrated to rails 4.2

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 # ruby '2.0.0'
 
-gem 'rails', '~> 4.1.1'
+gem 'rails', '~> 4.2'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Enable deflate / gzip compression of controller-generated responses
   config.middleware.use Rack::Deflater
