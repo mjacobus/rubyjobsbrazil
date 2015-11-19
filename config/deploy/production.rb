298e8e71409c3e840d6def1e3336a19b6aaf41ac
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-11-19 11:57:08 +0100

    Attempt to continuous deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,11 @@
 set :stage, :production
+set :branch, :master
 set :application, 'rubyjobsbrazil.com.br'
 set :deploy_to, "/home/rubyjobsbrazil/sites/#{fetch(:application)}"
 
 set :user, 'rubyjobsbrazil'
 set :password, ask('Password', nil)
+
 
 server 'rubyjobsbrazil.com.br',
   user: fetch(:user),
