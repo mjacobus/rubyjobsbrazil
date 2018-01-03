68fb8ac2acfee2f1d527d730a65874da8c9c52d1
Marcelo Jacobus <marcelo.jacobus@xing.com>
2018-01-03 10:53:49 +0100

    Without user context

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   layout 'recruiter/application'
-
-  before_action :set_raven_context
-
-  private
-
-  def set_raven_context
-    Raven.user_context(id: current_user.try(:id))
-    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
-  end
 end
