b63b0f5b49ba674e645fda90ea8300762f9610ea
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-05-10 12:15:01 -0300

    Fix some tests

--- spec/models/recruiter/oauth/github_spec.rb
+++ spec/models/recruiter/oauth/github_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Recruiter::Oauth::Github do
   include OmniauthModelSpecs
--- spec/models/recruiter/oauth/google_spec.rb
+++ spec/models/recruiter/oauth/google_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Recruiter::Oauth::Google do
   include OmniauthModelSpecs
