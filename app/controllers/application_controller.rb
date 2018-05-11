c7bd54dc9a846c774ab0dafe8887b2e309aa3690
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-05-11 17:40:29 -0300

    Fixup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include Recruiter::CrudFlashMessagerHelper
+  include CrudFlashMessagerHelper
 
   layout 'recruiter/application'
 
