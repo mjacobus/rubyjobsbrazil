fa03593031d9ef626ff3689548f55f44102d3526
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-05-11 17:41:54 -0300

    Rename file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -267,7 +267,7 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   config.warden do |manager|
-    manager.failure_app = Recruiter::CustomDeviseFailure
+    manager.failure_app = CustomDeviseFailure
   end
 end
 
