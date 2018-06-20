ebe3688bd98d316e0792cb60061a0721fa5936e5
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-06-20 10:33:38 +0200

    Fixup

--- deploy/environments/production.rb
+++ deploy/environments/production.rb
@@ -3,9 +3,9 @@
 set :deploy_to, "/var/www/apps/#{fetch(:application)}"
 
 server fetch(:application),
-  roles: %w[web app],
-  ssh_options: {
-    user: 'deploy', # overrides user setting above
-    forward_agent: true,
-    auth_methods: %w[publickey]
-  }
+       roles: %w[web app],
+       ssh_options: {
+         user: 'deploy', # overrides user setting above
+         forward_agent: true,
+         auth_methods: %w[publickey]
+       }
--- deploy/environments/staging.rb
+++ deploy/environments/staging.rb
@@ -1,10 +1,11 @@
-set :deploy_to, "/var/www/apps/staging.#{fetch(:application)}"
+# frozen_string_literal: true
+
 set :deploy_to, "/var/www/apps/staging.#{fetch(:application)}"
 
 server "staging.#{fetch(:application)}",
-  roles: %w[web app],
-  ssh_options: {
-    user: 'deploy', # overrides user setting above
-    forward_agent: true,
-    auth_methods: %w[publickey]
-  }
+       roles: %w[web app],
+       ssh_options: {
+         user: 'deploy', # overrides user setting above
+         forward_agent: true,
+         auth_methods: %w[publickey]
+       }
